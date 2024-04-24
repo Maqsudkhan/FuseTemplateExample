81627e9593221b55fc40591e4a9e5670dbda0466
Maqsudkhan <maqsudtorayev2003@gmail.com>
2024-04-24 09:33:10 +0500

    'added'

---
+++ labs-starter/src/app/app.routes.ts
@@ -0,0 +1,93 @@
+import { Route } from '@angular/router';
+import { initialDataResolver } from 'app/app.resolvers';
+import { AuthGuard } from 'app/core/auth/guards/auth.guard';
+import { NoAuthGuard } from 'app/core/auth/guards/noAuth.guard';
+import { LayoutComponent } from 'app/layout/layout.component';
+import { PricingSingleComponent } from './modules/admin/pages/pricing/single/single.component';
+
+// @formatter:off
+/* eslint-disable max-len */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+export const appRoutes: Route[] = [
+
+    // Redirect empty path to '/example'
+    {path: '', pathMatch : 'full', redirectTo: 'example'},
+
+    // Redirect signed-in user to the '/example'
+    //
+    // After the user signs in, the sign-in page will redirect the user to the 'signed-in-redirect'
+    // path. Below is another redirection for that path to redirect the user to the desired
+    // location. This is a small convenience to keep all main routes together here on this file.
+    {path: 'signed-in-redirect', pathMatch : 'full', redirectTo: 'example'},
+
+    // Auth routes for guests
+    {
+        path: '',
+        canActivate: [NoAuthGuard],
+        canActivateChild: [NoAuthGuard],
+        component: LayoutComponent,
+        data: {
+            layout: 'empty'
+        },
+        children: [
+            {path: 'confirmation-required', loadChildren: () => import('app/modules/auth/confirmation-required/confirmation-required.routes')},
+            {path: 'forgot-password', loadChildren: () => import('app/modules/auth/forgot-password/forgot-password.routes')},
+            {path: 'reset-password', loadChildren: () => import('app/modules/auth/reset-password/reset-password.routes')},
+            {path: 'sign-in', loadChildren: () => import('app/modules/auth/sign-in/sign-in.routes')},
+            {path: 'sign-up', loadChildren: () => import('app/modules/auth/sign-up/sign-up.routes')}
+        ]
+    },
+
+    // Auth routes for authenticated users
+    {
+        path: '',
+        canActivate: [AuthGuard],
+        canActivateChild: [AuthGuard],
+        component: LayoutComponent,
+        data: {
+            layout: 'empty'
+        },
+        children: [
+            {path: 'sign-out', loadChildren: () => import('app/modules/auth/sign-out/sign-out.routes')},
+            {path: 'unlock-session', loadChildren: () => import('app/modules/auth/unlock-session/unlock-session.routes')}
+        ]
+    },
+
+    // Landing routes
+    {
+        path: '',
+        component: LayoutComponent,
+        data: {
+            layout: 'empty'
+        },
+        children: [
+            {path: 'home', loadChildren: () => import('app/modules/landing/home/home.routes')},
+        ]
+    },
+
+    // Admin routes
+    {
+        path: '',
+        canActivate: [AuthGuard],
+        canActivateChild: [AuthGuard],
+        component: LayoutComponent,
+        resolve: {
+            initialData: initialDataResolver
+        },
+        children: [
+            {path: 'example', loadChildren: () => import('app/modules/admin/example/example.routes')},
+            // {path: 'pricing', loadChildren: () => import('app/modules/admin/pages/pricing/modern/modern.routes')},
+            // {path: 'pricing', loadChildren: () => import('app/modules/admin/pages/pricing/simple/simple.routes')},
+            // {path: 'pricing', loadChildren: () => import('app/modules/admin/pages/pricing/single/single.routes')},
+            // {path: 'pricing', loadChildren: () => import('app/modules/admin/pages/pricing/table/table.routes')},
+            {path: 'pricing', 
+            component:PricingSingleComponent,
+            children: [
+                {path: 'modern', loadChildren: () => import('app/modules/admin/pages/pricing/modern/modern.routes')},
+                {path: 'simple', loadChildren: () => import('app/modules/admin/pages/pricing/simple/simple.routes')},
+                {path: 'single', loadChildren: () => import('app/modules/admin/pages/pricing/single/single.routes')},
+                {path: 'table', loadChildren: () => import('app/modules/admin/pages/pricing/table/table.routes')}
+            ]},
+        ]
+    }
+];
---
+++ labs-starter/src/app/mock-api/common/navigation/data.ts
@@ -0,0 +1,206 @@
+/* eslint-disable */
+import { FuseNavigationItem } from '@fuse/components/navigation';
+
+export const defaultNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example' 
+    },
+
+    {
+        id      : 'pages',
+        title   : 'Pages',
+        subtitle: 'Custom made page designs',
+        type    : 'group',
+        icon    : 'heroicons_outline:document',
+        children: [
+
+
+            {
+                id      : 'pages.pricing',
+                title   : 'Pricing',
+                type    : 'collapsable',
+                icon    : 'heroicons_outline:banknotes',
+                children: [
+                    {
+                        id   : 'pages.pricing.modern',
+                        title: 'Modern',
+                        type : 'basic',
+                        link: '/pages/pricing/simple',
+                    },
+                    {
+                        id   : 'pages.pricing.simple',
+                        title: 'Simple',
+                        type : 'basic',
+                        link : '/pages/pricing/simple',
+                    },
+                    {
+                        id   : 'pages.pricing.single',
+                        title: 'Single',
+                        type : 'basic',
+                        link : '/pages/pricing/single',
+                    },
+                    {
+                        id   : 'pages.pricing.table',
+                        title: 'Table',
+                        type : 'basic',
+                        link : '/pages/pricing/table',
+                    },
+                ],
+            },
+        ],
+    }
+];
+
+    
+export const compactNavigation: FuseNavigationItem[] = [
+    {
+        id      : 'pages',
+        title   : 'Pages',
+        subtitle: 'Custom made page designs',
+        type    : 'group',
+        icon    : 'heroicons_outline:document',
+        children: [
+            {
+                id      : 'pages.pricing',
+                title   : 'Pricing',
+                type    : 'collapsable',
+                icon    : 'heroicons_outline:banknotes',
+                children: [
+                    {
+                        id   : 'pages.pricing.modern',
+                        title: 'Modern',
+                        type : 'basic',
+                        link : 'pages/pricing/modern',
+                    },
+                    {
+                        id   : 'pages.pricing.simple',
+                        title: 'Simple',
+                        type : 'basic',
+                        link : 'pages/pricing/simple',
+                    },
+                    {
+                        id   : 'pages.pricing.single',
+                        title: 'Single',
+                        type : 'basic',
+                        link : '/pages/pricing/single',
+                    },
+                    {
+                        id   : 'pages.pricing.table',
+                        title: 'Table',
+                        type : 'basic',
+                        link : '/pages/pricing/table',
+                    },
+                ],
+            },
+        ],
+    }
+];
+
+export const futuristicNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example' 
+    },
+
+    {
+        id      : 'pages',
+        title   : 'Pages',
+        subtitle: 'Custom made page designs',
+        type    : 'group',
+        icon    : 'heroicons_outline:document',
+        children: [
+            {
+                id      : 'pages.pricing',
+                title   : 'Pricing',
+                type    : 'collapsable',
+                icon    : 'heroicons_outline:banknotes',
+                children: [
+                    {
+                        id   : 'pages.pricing.modern',
+                        title: 'Modern',
+                        type : 'basic',
+                        link : '/pages/pricing/modern',
+                    },
+                    {
+                        id   : 'pages.pricing.simple',
+                        title: 'Simple',
+                        type : 'basic',
+                        link : '/pages/pricing/simple',
+                    },
+                    {
+                        id   : 'pages.pricing.single',
+                        title: 'Single',
+                        type : 'basic',
+                        link : '/pages/pricing/single',
+                    },
+                    {
+                        id   : 'pages.pricing.table',
+                        title: 'Table',
+                        type : 'basic',
+                        link : '/pages/pricing/table',
+                    },
+                ],
+            },
+        ],
+    }
+];
+export const horizontalNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example' 
+    },
+
+    {
+        id      : 'pages',
+        title   : 'Pages',
+        subtitle: 'Custom made page designs',
+        type    : 'group',
+        icon    : 'heroicons_outline:document',
+        children: [
+
+
+            {
+                id      : 'pages.pricing',
+                title   : 'Pricing',
+                type    : 'collapsable',
+                icon    : 'heroicons_outline:banknotes',
+                children: [
+                    {
+                        id   : 'pages.pricing.modern',
+                        title: 'Modern',
+                        type : 'basic',
+                        link : '/pages/pricing/modern',
+                    },
+                    {
+                        id   : 'pages.pricing.simple',
+                        title: 'Simple',
+                        type : 'basic',
+                        link : '/pages/pricing/simple',
+                    },
+                    {
+                        id   : 'pages.pricing.single',
+                        title: 'Single',
+                        type : 'basic',
+                        link : '/pages/pricing/single',
+                    },
+                    {
+                        id   : 'pages.pricing.table',
+                        title: 'Table',
+                        type : 'basic',
+                        link : '/pages/pricing/table',
+                    },
+                ],
+            },
+        ],
+    }
+];
